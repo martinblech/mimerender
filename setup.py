f3c632eb10d88692c4d14031f4e6084e192c86f6
martinblech <martinblech@08fc50c4-2f68-11de-9c63-4752e237b823>
2009-05-20 14:10:55 +0000

    updated version number
    git-svn-id: https://mimerender.googlecode.com/svn/trunk@14 08fc50c4-2f68-11de-9c63-4752e237b823

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='mimerender',
-    version='0.1',
+    version='0.2',
     description='RESTful resource variant rendering using MIME Media-Types',
     author='Martin Blech',
     author_email='mblech@bmat.com',
