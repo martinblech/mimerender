039c9b756a21bbdee6a089721a40cb0250f99b0b
Martin Blech <martinblech@gmail.com>
2012-08-23 00:52:44 -0500

    reverting to original mimeparse dependency, py3k will have to wait

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
     ],
     py_modules=['mimerender'],
     package_dir={'':'src'},
-    requires=['python-mimeparse'],
-    install_requires=['python-mimeparse'],
+    requires=['mimeparse'],
+    install_requires=['mimeparse'],
 )
