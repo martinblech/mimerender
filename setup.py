00ac8dcae3696646d7a68a4e2684ee64a95f9513
Patrick Huck <phuck@lbl.gov>
2021-08-06 21:08:31 -0700

    testing and release GH actions

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 from setuptools import setup
 
 setup(
-    name='mimerender',
-    version='0.6.0',
+    name='mimerender-pr36',
+    version='0.6.1',
     description='RESTful HTTP Content Negotiation for Flask, Bottle, web.py '
         'and webapp2 (Google App Engine)',
     author='Martin Blech',
     author_email='martinblech@gmail.com',
     url='https://github.com/martinblech/mimerender',
     license='MIT',
-    long_description="""This module provides a decorator that wraps a HTTP
+    long_description="""Fork for https://github.com/martinblech/mimerender/pull/36.
+    This module provides a decorator that wraps a HTTP
     request handler to select the correct render function for a given HTTP
     Accept header. It uses mimeparse to parse the accept string and select the
     best available representation. Supports Flask, Bottle, web.py and webapp2
